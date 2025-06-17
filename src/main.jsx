16e4a165790ddefcaae9288312104691185bed48
JorgeMabuie <jorgemaabuie@gmail.com>
2025-06-17 11:07:12 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,16 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import { SocketProvider } from './context/SocketContext';
+import axios from 'axios';
+
+// Configura interceptor para injetar o token JWT em todas as requests
+axios.interceptors.request.use(config => {
+  const token = localStorage.getItem('token');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+}, error => Promise.reject(error));
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
